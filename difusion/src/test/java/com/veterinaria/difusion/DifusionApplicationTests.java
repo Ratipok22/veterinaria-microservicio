0d6d965b9e0ae926c2ad6d2f45f8a32c3b429ab9
Bruno Molina <133899555+Ratipok22@users.noreply.github.com>
2026-05-04 01:25:59 -0400

    Creación microservicio difusion

---
+++ difusion/src/test/java/com/veterinaria/difusion/DifusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.difusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

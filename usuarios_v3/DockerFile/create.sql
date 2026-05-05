1f4d262246545c9cbdddbaeab0906a0cc35e2591
Bruno <bru.molina@duocuc.cl>
2026-05-05 19:13:03 -0400

    Limpieza de versiones duplicadas y archivos temporales_v2

--- usuarios_v3/DockerFile/create.sql
+++
@@ -1,3 +0,0 @@
-create database mydatabase;
-create user 'myuser'@'%' identified by 'password';
-grant all on mydatabase.* to 'myuser'@'%';
--- usuarios_v3/src/main/java/com/duoc/backend/Usuarios/UsuariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.duoc.backend.Usuarios;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.duoc.backend.Usuarios.Usuarios;
-
-public interface UsuariosRepository extends CrudRepository<Usuarios, Long> {
-}

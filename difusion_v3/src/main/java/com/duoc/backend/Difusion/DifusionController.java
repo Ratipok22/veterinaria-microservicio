444ca7332f706da502594f5ecde1d84264f10b15
Bruno Molina <133899555+Ratipok22@users.noreply.github.com>
2026-06-22 02:09:54 -0400

    Actualización microservicio difusion_v3

--- difusion_v3/src/main/java/com/duoc/backend/Difusion/DifusionController.java
+++ difusion_v3/src/main/java/com/duoc/backend/Difusion/DifusionController.java
@@ -3,6 +3,8 @@ package com.duoc.backend.Difusion;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -29,13 +34,17 @@ public class DifusionController {
     }
 
     @GetMapping("/{id}")
-    public Difusion obtenerDifusionPorId(@PathVariable Long id) {
-        return difusionService.obtenerDifusionPorId(id);
+    public ResponseEntity<Difusion> obtenerDifusionPorId(@PathVariable Long id) {
+        Difusion difusion = difusionService.obtenerDifusionPorId(id);
+        if (difusion == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(difusion);
     }
 
     @PostMapping
-    public Difusion guardarDifusion(@RequestBody Difusion difusion) {
-        return difusionService.guardarDifusion(difusion);
+    public ResponseEntity<Difusion> guardarDifusion(@Valid @RequestBody Difusion difusion) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(difusionService.guardarDifusion(difusion));
     }
 
     @DeleteMapping("/{id}")
